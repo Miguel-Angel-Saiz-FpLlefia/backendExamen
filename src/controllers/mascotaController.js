9706939f9050a13158ab278f8709a18895e7586f
daw2-miguelangelsaiz26 <saizangullomiguelangel@fpllefia.com>
2026-04-22 16:58:55 +0200

    Crear el crud completo

--- src/controllers/mascotaController.js
+++ src/controllers/mascotaController.js
@@ -31,4 +31,38 @@ function getMascotaById(req, res) {
   res.json(mascotas[index]);
 }
 
-export { getAllMascotas, getMascotaById };
+function crearMascota(req, res) {
+  const { nombre, tipo, raza, foto } = req.body;
+  const id = String(mascotas.length + 1);
+  const nova = { id, nombre, tipo, raza, foto };
+  mascotas.push(nova);
+  res.status(201).json({ mascota: nova });
+}
+
+function editarMascota(req, res) {
+  const { id } = req.params;
+  const index = mascotas.findIndex((c) => c.id === id);
+  if (index === -1) {
+    return res.status(404).json({ error: "Mascota no encontrada", id });
+  }
+  mascotas[index] = { ...mascotas[index], ...req.body, id };
+  res.json({ mascota: mascotas[index] });
+}
+
+function eliminarMascota(req, res) {
+  const { id } = req.params;
+  const index = mascotas.findIndex((c) => c.id === id);
+  if (index === -1) {
+    return res.status(404).json({ error: "Mascota no encontrada", id });
+  }
+  mascotas.splice(index, 1);
+  res.status(204).send();
+}
+
+export {
+  getAllMascotas,
+  getMascotaById,
+  crearMascota,
+  editarMascota,
+  eliminarMascota,
+};
--- src/routes/mascotasRoutes.js
+++ src/routes/mascotasRoutes.js
@@ -2,11 +2,17 @@ import express from "express";
 import {
   getAllMascotas,
   getMascotaById,
+  crearMascota,
+  editarMascota,
+  eliminarMascota,
 } from "../controllers/mascotaController.js";
 
 const mascotasRouter = express.Router();
 
 mascotasRouter.get("/", getAllMascotas);
 mascotasRouter.get("/:id", getMascotaById);
+mascotasRouter.post("/", crearMascota);
+mascotasRouter.put("/:id", editarMascota);
+mascotasRouter.delete("/:id", eliminarMascota);
 
 export default mascotasRouter;
