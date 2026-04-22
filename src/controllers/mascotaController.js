c23c2549813840e25f435e0ccbac409303f0a981
daw2-miguelangelsaiz26 <saizangullomiguelangel@fpllefia.com>
2026-04-22 18:52:42 +0200

    crear el crud con array

--- src/controllers/mascotaController.js
+++ src/controllers/mascotaController.js
@@ -1,94 +1,65 @@
 import Mascota from "../models/mascotaModel.js";
 import mongoose from "mongoose";
 
-// const mascotas = [
-//   {
-//     id: "1",
-//     nombre: "Labrador",
-//     tipo: "Perro",
-//     raza: "Labrador",
-//     foto: "https://imagenMascota1.jpg",
-//   },
-//   {
-//     id: "2",
-//     nombre: "Chihuhua",
-//     tipo: "Perro",
-//     raza: "Chihuhua",
-//     foto: "https://imagenMascota2.jpg",
-//   },
-// ];
+const mascotas = [
+  {
+    id: "1",
+    nombre: "Labrador",
+    tipo: "Perro",
+    raza: "Labrador",
+    foto: "https://imagenMascota1.jpg",
+  },
+  {
+    id: "2",
+    nombre: "Chihuhua",
+    tipo: "Perro",
+    raza: "Chihuhua",
+    foto: "https://imagenMascota2.jpg",
+  },
+];
 
-async function getAllMascotas(req, res) {
-  try {
-    const dades = await Mascota.find().sort({ createdAt: -1 });
-    res.json({
-      dades: dades,
-      total: dades.length,
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+function getAllMascotas(req, res) {
+  res.json({
+    dades: mascotas,
+    total: mascotas.length,
+  });
 }
 
-async function getMascotaById(req, res) {
-  try {
-    const { id } = req.params;
-    const dada = await Mascota.findById(id);
-    res.json({
-      dada,
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+function getMascotaById(req, res) {
+  const { id } = req.params;
+  const mascota = mascotas.findIndex((c) => c.id === id);
+  if (mascota === -1) {
+    return res.json({ error: "Mascota no encontrada", id });
   }
+  res.json(mascota);
 }
 
-async function crearMascota(req, res) {
-  try {
-    const { nombre, tipo, raza, foto } = req.body;
-    if (!nombre || !tipo || !raza || !foto) {
-      return res.json(400).json({ error: "Faltan datos por introducir" });
-    }
-    const mascota = await Mascota.create({ nombre, tipo, raza, foto });
-    res.status(201).json({ mascota: nova });
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+function crearMascota(req, res) {
+  const { nombre, tipo, raza, foto } = req.body;
+  const { id } = req.params.id;
+  const nova = { id, nombre, tipo, raza, foto };
+  mascotas.push(nova);
+  res.status(201).json(nova);
 }
 
-async function editarMascota(req, res) {
-  try {
-    const mascotaActual = await Mascota.findById(req.params.id);
-    if (!mascotaActual) {
-      return res.status(404).json({ error: "Mascota no encontrada", id });
-    }
-    const actualitzada = await Mascota.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-    );
-    res.status(200).json({
-      mascota: actualitzada,
-    });
-  } catch (err) {
-    res.status(400).json({
-      error: err.message,
-    });
+function editarMascota(req, res) {
+  const { id } = req.params;
+  const index = mascotas.findIndex((c) => c.id === id);
+  if (index === -1) {
+    return res.status(404).json({ error: "No se ha encontrado la mascota" });
   }
+  mascotas[index] = { ...mascotas[index], ...req.body, id };
+  res.josn(mascotas[index]);
 }
 
-async function eliminarMascota(req, res) {
-  try {
-    const { id } = req.params;
-    const eliminada = await Mascota.findByIdAndDelete(id);
-    if (!eliminada) {
-      return res.status(404).json({
-        error: "Usuario no encontrado",
-        id: id,
-      });
-    }
-    res.status(204).send();
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+function eliminarMascota(req, res) {
+  const { id } = req.params;
+  const index = mascotas.findIndex((c) => c.id === id);
+  if (index === -1) {
+    return res.status(404).json({ error: "No se ha encontrado la mascota" });
   }
+  mascotas.splice(index, 1);
+  res.status(204).send();
 }
 
 export {
